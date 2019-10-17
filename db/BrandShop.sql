7d752af3d191d67bf15d2091653c7f47ef159dc6
cheefs <cheefst@outlook.com>
2019-10-17 22:26:21 +0700

    разработка базы данных для магазина

---
+++ db/BrandShop.sql
@@ -0,0 +1,163 @@
+DROP DATABASE IF EXISTS brand_shop;
+CREATE DATABASE brand_shop;
+USE brand_shop;
+
+/*
+ Создание базовой таблици пользователей, при регистрации заполняется в первую очередь она
+*/
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+    email varchar(128) unique NOT null comment 'Почта пользователя',
+    `password` varchar(256) NOT NULL COMMENT 'Пароль',
+    is_deleted boolean default false comment 'Флаг активности записи',
+    create_date timestamp default now(),
+    modify_date timestamp default now(),
+    INDEX user_email(email)
+) comment 'Пользователи интернет магазина';
+
+INSERT INTO users (email, `password`) VALUES 
+( 'test@mail.test', '11111' ),
+( 'test1@mail.test', '55555' ),
+( 'test2@mail.test', '44444' ),
+( 'test3@mail.test', '33333' );
+
+CREATE TRIGGER on_users_update before UPDATE
+ON users FOR EACH ROW 
+set NEW.modify_date = now();
+
+update users u set u.email = 'updated@mail.upd' WHERE id = 1; 
+Select * from users;
+
+/*
+	Таблица дополнительной информации о пользователях
+*/
+DROP TABLE IF EXISTS users_profiles;
+CREATE TABLE users_profiles (
+	id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+    user_id BIGINT UNSIGNED not null unique comment 'Сязь с таблицей пользователей',
+	first_name varchar(128) NOT null comment 'Имя',
+    second_name varchar(128) comment 'Отчество',
+    last_name varchar(128) Not null comment 'Фамилия',
+    bio text COMMENT 'О себе',
+    phone varchar(12) not null comment 'Телефон',
+    
+	create_date timestamp default now(),
+	modify_date timestamp default now(),
+	create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+	modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+
+	INDEX user_id_inx(user_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    	ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+        
+) comment 'Профили пользователей';
+
+/* 
+	Группы для пользователей, чтоб было проще раздавать привелегии для них
+*/
+DROP TABLE IF EXISTS `groups`;
+CREATE TABLE `groups` (
+	id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+    `name` varchar(256) comment '',
+	is_deleted boolean default false comment 'Флаг активности записи',
+	create_date timestamp default now(),
+	modify_date timestamp default now(),
+	create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+	modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+	INDEX `group_name`(`name`),
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+
+) comment 'Список всех групп';
+
+/* 
+	Действия ( если нужно будет группам отдельно накидывать права)
+*/
+DROP TABLE IF EXISTS actions;
+CREATE TABLE actions (
+	id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+    action_name varchar(256) not null unique comment 'Название действия',
+    is_deleted boolean default false comment 'Флаг активности записи',
+	INDEX `action_name_inx`(`action_name`)
+) comment 'Таблица действий';
+
+/* 
+	Действия которые доступны группам
+*/
+DROP TABLE IF EXISTS group_actions;
+CREATE TABLE group_actions (
+	group_id bigint unsigned not null unique comment 'Указатель на группу',
+    action_id bigint unsigned not null unique comment 'Указатель на действие',
+
+	INDEX `group_id_inx`(`group_id`),
+    INDEX `action_id_inx`(`action_id`),
+    FOREIGN KEY (group_id) REFERENCES `groups`(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (action_id) REFERENCES `actions`(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Таблица связки действий и групп';
+
+/* 
+	Связь пользователя с определенной группой
+*/
+DROP TABLE IF EXISTS users_groups;
+CREATE TABLE users_groups (
+    user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя',
+    group_id BIGINT UNSIGNED not null unique comment 'Указатель на группу пользователя',
+    
+	INDEX `user_id_inx`(`user_id`),
+    INDEX `group_id_inx`(`group_id`),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    	ON UPDATE CASCADE ON DELETE restrict,
+	 FOREIGN KEY (group_id) REFERENCES `groups`(id)
+    	ON UPDATE CASCADE ON DELETE restrict
+) comment 'Таблица связки групп и пользователей';
+
+/*
+  История авторизаций пользователя, нужна будет для сбора статистики
+*/
+DROP TABLE IF EXISTS login_history;
+CREATE TABLE login_history (
+    user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя',
+    login_time timestamp default now() comment 'Время последней авторизациина сайте',
+    ip_address varchar(256) comment 'IP адресс пользователя с которой производитась авторизация',
+    browser varchar(50) comment 'Браузер пользователя с которого он был авторизован',
+    
+	INDEX `user_id_inx`(`user_id`),
+    INDEX `ip_address_inx`(`ip_address`),
+	INDEX `browser_inx`(`browser`),
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'История авторизаций';
+
+/* Далее таблици которые используются непосредственно в работе магазина */
+
+/*
+   Категории товаров
+*/
+DROP TABLE IF EXISTS products_categories;
+CREATE TABLE products_categories (
+	 id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     category_name varchar(256) not null unique comment 'Название категории товаров',
+     is_deleted boolean default false comment 'Флаг активности записи',
+    
+     create_date timestamp default now(),
+     modify_date timestamp default now(),
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	INDEX `category_name_inx`(`category_name`),
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Категории товаров';
+
