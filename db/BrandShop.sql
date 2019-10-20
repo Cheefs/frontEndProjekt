9daf486d5d69ebaa19cee84ca782d51ca0b06c66
cheefs <cheefst@outlook.com>
2019-10-20 18:09:50 +0700

    добавленны таблици магазина

--- db/BrandShop.sql
+++ db/BrandShop.sql
@@ -11,8 +11,8 @@ CREATE TABLE users (
     email varchar(128) unique NOT null comment 'Почта пользователя',
     `password` varchar(256) NOT NULL COMMENT 'Пароль',
     is_deleted boolean default false comment 'Флаг активности записи',
-    create_date timestamp default now(),
-    modify_date timestamp default now(),
+    create_date timestamp default now() comment 'Дата создания записи',
+	modify_date timestamp default now() comment 'Дата изменения записи',
     INDEX user_email(email)
 ) comment 'Пользователи интернет магазина';
 
@@ -42,8 +42,8 @@ CREATE TABLE users_profiles (
     bio text COMMENT 'О себе',
     phone varchar(12) not null comment 'Телефон',
     
-	create_date timestamp default now(),
-	modify_date timestamp default now(),
+    create_date timestamp default now() comment 'Дата создания записи',
+	modify_date timestamp default now() comment 'Дата изменения записи',
 	create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
 	modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
 
@@ -65,8 +65,8 @@ CREATE TABLE `groups` (
 	id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
     `name` varchar(256) comment '',
 	is_deleted boolean default false comment 'Флаг активности записи',
-	create_date timestamp default now(),
-	modify_date timestamp default now(),
+    create_date timestamp default now() comment 'Дата создания записи',
+	modify_date timestamp default now() comment 'Дата изменения записи',
 	create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
 	modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
 	INDEX `group_name`(`name`),
@@ -149,8 +149,8 @@ CREATE TABLE products_categories (
      category_name varchar(256) not null unique comment 'Название категории товаров',
      is_deleted boolean default false comment 'Флаг активности записи',
     
-     create_date timestamp default now(),
-     modify_date timestamp default now(),
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
      create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
      modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
     
@@ -160,4 +160,141 @@ CREATE TABLE products_categories (
 	FOREIGN KEY (modify_user_id) REFERENCES users(id)
 		ON UPDATE CASCADE ON DELETE restrict
 ) comment 'Категории товаров';
+
+/* Таблица товаров магазина */
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+	 id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     category_id bigint unsigned unique comment 'указание на категорию товаров',
+     `name` boolean default false comment 'Название товара',
+     is_deleted bool DEFAULT false comment 'Флаг активности записи', 
+ 
+	 create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	INDEX `category_id_inx`(`category_id`),
+	FOREIGN KEY (category_id) REFERENCES products_categories(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Категории товаров';
+
+/** справочник размеров */
+DROP TABLE IF EXISTS sizes;
+CREATE TABLE sizes (
+	 id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     name varchar(256) comment 'Название размера',
+     is_deleted bool DEFAULT false comment 'Флаг активности записи', 
+     
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Справочник размеров';
+
+/** справочник цветов */
+DROP TABLE IF EXISTS colors;
+CREATE TABLE colors (
+     id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     name varchar(256) comment 'Название цвета',
+	 is_deleted bool DEFAULT false comment 'Флаг активности записи', 
+
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Справочник цветов';
+
+/** справочник брендов\производителей */
+DROP TABLE IF EXISTS brands;
+CREATE TABLE brands (
+     id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     name varchar(256) comment 'Название Бренда\производитель',
+     logo varchar(256) comment 'Ссылка на файл с логотипом',
+	 is_deleted bool DEFAULT false comment 'Флаг активности записи', 
+
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Справочник брендов\производителей';
+
+
+/** справочник возварастных групп */
+DROP TABLE IF EXISTS age_groups;
+CREATE TABLE age_groups (
+     id SERIAL PRIMARY KEY COMMENT 'Первичный ключь',
+     name varchar(256) comment 'Название возврастной группы',
+     min_age int unsigned not null comment 'Минимальный возвраст',
+     max_age int unsigned comment 'Максимальный возвраст',
+	 is_deleted bool DEFAULT false comment 'Флаг активности записи', 
+
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+    
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Справочник возварастных групп';
+
+/* таблица хранит в себе все о товаре, */
+DROP TABLE IF EXISTS product_details;
+CREATE TABLE product_details (
+     product_id bigint unsigned unique not null comment 'указание на категорию товаров',
+     size_id bigint unsigned unique not null comment 'указатель на размер',
+     color_id bigint unsigned unique not null comment 'указатель на цвет',
+	 brand_id bigint unsigned unique not null comment 'указатель на бренд производитель',
+     age_group_id bigint unsigned unique not null comment 'указатель на возврастную группу',
+          sex ENUM('man', 'woman') not null comment 'Пол',
+	 `desc` varchar(256) comment 'описание товара',
+	 price bigint unsigned comment 'Цена товара', 
+     
+     create_date timestamp default now() comment 'Дата создания записи',
+     modify_date timestamp default now() comment 'Дата изменения записи',
+     create_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который создал запись',
+     modify_user_id BIGINT UNSIGNED not null unique comment 'Указатель на пользователя который отредактировал запись',
+     
+	INDEX `sex_inx`(`sex`),
+	INDEX `product_id_inx`(`product_id`),
+	INDEX `size_id_inx`(`size_id`),
+	INDEX `color_id_inx`(`color_id`),
+    INDEX `brand_id_inx`(`brand_id`),
+	INDEX `age_group_id_inx`(`age_group_id`),
+	FOREIGN KEY (product_id) REFERENCES products(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (size_id) REFERENCES sizes(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (color_id) REFERENCES colors(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (brand_id) REFERENCES brands(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (age_group_id) REFERENCES age_groups(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (create_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict,
+	FOREIGN KEY (modify_user_id) REFERENCES users(id)
+		ON UPDATE CASCADE ON DELETE restrict
+) comment 'Таблица полной информации о товаре';
 
